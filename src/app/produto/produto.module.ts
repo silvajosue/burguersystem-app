ce985a3bc318b0cd6c230315b81ba50677aba512
erick <erickpf.2001@gmail.com>
2021-09-16 21:02:55 -0300

    3

--- src/app/produto/produto.module.ts
+++ src/app/produto/produto.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProdutoRoutingModule } from './produto-routing.module';
+import { CadastrarProdutoComponent } from './cadastrar-produto/cadastrar-produto.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CadastrarProdutoComponent
+
+  }
+];
 
 
 @NgModule({
