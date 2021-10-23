40b74df344a7535056558d5a4f919a0bfcbf4a7f
EmersonRDJ <90483020+EmersonRDJ@users.noreply.github.com>
2021-10-22 22:37:27 -0300

    Cadastro de Receita

---
+++ src/app/dto/ComposicaoDTO.ts
@@ -0,0 +1,7 @@
+export class ComposicaoDTO{
+    public id: Number;
+    public codProduto: Number;
+    public codMaterial: Number;
+    public quantidade: Number;
+    public medida: String;
+}
--- src/app/receita/pages/cadreceita/cadreceita.component.ts
+++ src/app/receita/pages/cadreceita/cadreceita.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ReceitaDTO } from 'src/app/dto/ReceitaDTO';
+import { ProdutoDTO } from 'src/app/produto/model/ProdutoDTO';
+import { ComposicaoDTO } from 'src/app/dto/ComposicaoDTO';
 import { UsuarioDTO } from 'src/app/login/model/UsuarioDTO';
 
 @Component({
@@ -10,9 +12,19 @@ import { UsuarioDTO } from 'src/app/login/model/UsuarioDTO';
 })
 export class CadreceitaComponent implements OnInit {
 
-  public receita: ReceitaDTO;
+  public receita: ProdutoDTO;
+  public composicao: ComposicaoDTO;
   private usuario: UsuarioDTO;
   formulario: FormGroup;
+  
+  composicoes = [];
+
+  /*public composicao: {
+    ingrediente: string,
+    quantidade: number,
+    medida: string
+  };*/
+
   
   constructor(private formBuilder: FormBuilder) { }
 
@@ -22,28 +34,39 @@ export class CadreceitaComponent implements OnInit {
 
   public createForm(): void {
     this.formulario = this.formBuilder.group({
+      codCategoria: [],
       nome: [],
       preco: [],
       foto: [],
-      ingrediente: [],
+      codMaterial: [],
       quantidade: [],
       medida: [],  
     });
   }
 
   public cadastrar(): void {
-    this.receita = new ReceitaDTO();
+    this.receita = new ProdutoDTO();
     console.log(String(this.formulario.get('nome').value))
+    this.receita.codCategoria = Number(this.formulario.get('codCategoria').value);
+    console.log(this.formulario.get('codCategoria').value);
     this.receita.nome = String(this.formulario.get('nome').value);
     this.receita.preco = Number(this.formulario.get('preco').value);
     this.receita.foto = String(this.formulario.get('foto').value);
-    this.receita.ingrediente = String(this.formulario.get('ingrediente').value);
-    this.receita.quantidade = Number(this.formulario.get('quantidade').value);
-    this.receita.medida = String(this.formulario.get('medida').value);
 
-    console.log(this.formulario.get('ingrediente').value);
-    console.log(this.receita)
     alert("ok");
+  }
+
+  public inserir(): void {
+    this.composicao = new ComposicaoDTO();
+    this.composicao.codMaterial = Number(this.formulario.get('codMaterial').value);
+    console.log(this.formulario.get('codMaterial').value);
+    this.composicao.quantidade = Number(this.formulario.get('quantidade').value);
+    this.composicao.medida = String(this.formulario.get('medida').value);
+    console.log(this.formulario.get('codMaterial').value);
+
+    this.composicoes.push(this.composicao);
+    console.log(this.composicoes);
+    alert("Componente cadastrado!");
   }
 
   public limpar(): void {
@@ -52,3 +75,4 @@ export class CadreceitaComponent implements OnInit {
   }
 
 }
+
