c0446f95ea4db698aa08d762c465a13e72869e73
EmersonRDJ <90483020+EmersonRDJ@users.noreply.github.com>
2021-11-05 19:38:10 -0300

    arrumando o erro cannot get

--- src/app/pedido/pages/cadpedido/cadpedido.component.ts
+++ src/app/pedido/pages/cadpedido/cadpedido.component.ts
@@ -14,15 +14,15 @@ export class CadpedidoComponent implements OnInit {
   //constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.createForm();
+    //this.createForm();
   }
-  public createForm(): void {
-      this.formulario = this.formBuilder.group({
-      cliente: [],
-      mesa: [],
-      //preco_total: [],
-    });
-  }
+  // public createForm(): void {
+  //     this.formulario = this.formBuilder.group({
+  //     cliente: [],
+  //     mesa: [],
+  //     //preco_total: [],
+  //   });
+  // }
 
   
   public cadastrar(): void {
